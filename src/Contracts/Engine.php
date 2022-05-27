c6a9c1d0a28169ec685e02cb1d7945261b174563
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-05-27 00:13:13 -0700

    updated

---
+++ src/Contracts/Engine.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Engine interface
+ *
+ * @package   Backdrop Core
+ * @copyright Copyright (C) 2019-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @author    Benjamin Lu ( https://benjlu.com )
+ */
+
+/**
+ * Define namespace
+ */
+namespace Benlumia007\Backdrop\Template\View\Contracts;
+
+/**
+ * Engine interface.
+ * 
+ * @since  1.0.0
+ * @access public
+ */
+interface Engine {
+    /**
+     * Returns a View object.
+     * 
+     * @since  1.0.0
+     * @access public
+     * @param  string $name
+     * @param  mixed  $slugs
+     * @return View
+     */
+    public function view( $name, $slugs = [] );
+
+    /**
+     * Outputs a view template.
+     * 
+     * @since  1.0.0
+     * @access public
+     * @param  string $name
+     * @param  mixed  $slugs
+     * @return void
+     */
+    public function display( $name, $slugs = [] );
+
+    /**
+     * Returns a view template as a string.
+     * 
+     * @since  1.0.0
+     * @access public
+     * @param  string $name
+     * @param  mixed  $slugs
+     * @return void
+     */
+    public function render( $name, $slugs = [] );
+}
