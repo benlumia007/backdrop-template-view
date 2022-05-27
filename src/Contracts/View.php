222402e8dc3fa878b8edfb70b9e012ae7b1111d3
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-05-27 00:33:01 -0700

    updated

--- src/Contracts/View.php
+++ src/Contracts/View.php
@@ -2,7 +2,7 @@
 /**
  * View interface
  *
- * @package   Backdrop Core
+ * @package   Backdrop
  * @copyright Copyright (C) 2019-2021. Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  * @author    Benjamin Lu ( https://benjlu.com )
