eb0c719240e67687359a7536a57d872acdb6990e
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-05-27 00:16:22 -0700

    updated

--- src/View/View/Provider.php
+++ src/View/View/Provider.php
@@ -13,7 +13,7 @@
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-namespace Benlumia007\Backdrop\Template\View;
+namespace Benlumia007\Backdrop\Template\View\View;
 
 use Benlumia007\Backdrop\Support\ServiceProvider;
 use Benlumia007\Backdrop\Template\View\Engine\Component as Engine;
